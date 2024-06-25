4208a1e8c032da751566f171968f74e224945111
HyperKonstantin <kglembotskij@gmail.com>
2024-06-25 20:58:55 +0300

    init

---
+++ src/main/java/sc/spring/natsListener/NatsListenerApplication.java
@@ -0,0 +1,13 @@
+package sc.spring.natsListener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sc/spring/natsListener/NatsListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package sc.spring.natsListener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatsListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
